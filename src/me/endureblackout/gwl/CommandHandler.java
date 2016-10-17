8190449b3f45a3de0d14c3bc5fe54b74d5825a63
EndureBlackout <jacobellingson@aol.com>
2016-10-17 11:54:28 -0500

    Better leaderboard @rayzr522 :P

--- src/me/endureblackout/gwl/CommandHandler.java
+++ src/me/endureblackout/gwl/CommandHandler.java
@@ -1,10 +1,18 @@
 
 package me.endureblackout.gwl;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.UUID;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -48,6 +56,40 @@ public class CommandHandler implements CommandExecutor {
 	public CommandHandler(GWL instance) {
 		core = instance;
 	}
+	
+    private static Map<UUID, Integer> sortByComparator(Map<UUID, Integer> unsortMap, final boolean order)
+    {
+
+        List<Entry<UUID, Integer>> list = new LinkedList<Entry<UUID, Integer>>(unsortMap.entrySet());
+
+        // Sorting the list based on values
+        Collections.sort(list, new Comparator<Entry<UUID, Integer>>()
+        {
+            public int compare(Entry<UUID, Integer> o1,
+                    Entry<UUID, Integer> o2)
+            {
+                if (order)
+                {
+                    return o1.getValue().compareTo(o2.getValue());
+                }
+                else
+                {
+                    return o2.getValue().compareTo(o1.getValue());
+
+                }
+            }
+
+        });
+
+        // Maintaining insertion order with the help of LinkedList
+        Map<UUID, Integer> sortedMap = new LinkedHashMap<UUID, Integer>();
+        for (Entry<UUID, Integer> entry : list)
+        {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedMap;
+    }
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -67,139 +109,17 @@ public class CommandHandler implements CommandExecutor {
 
 					if (args[0].equalsIgnoreCase("list")) {
 						p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8-=-=- (&9Wanted&8) -=-=-"));
-
-						for (OfflinePlayer t : Bukkit.getOfflinePlayers()) {
-
-							if (!LevelHandler.level.isEmpty()) {
-								if (LevelHandler.level.containsKey(t.getUniqueId())) {
-									int points = LevelHandler.level.get(t.getUniqueId());
-
-									if (points > top15 && !(t.getName().equals(p14))) {
-										if (points > top14 && !(t.getName().equals(p13))) {
-											if (points > top13 && !(t.getName().equals(p12))) {
-												if (points > top12 && !(t.getName().equals(p11))) {
-													if (points > top11 && !(t.getName().equals(p10))) {
-														if (points > top10 && !(t.getName().equals(p9))) {
-															if (points > top9 && !(t.getName().equals(p8))) {
-																if (points > top8 && !(t.getName().equals(p7))) {
-																	if (points > top7 && !(t.getName().equals(p6))) {
-																		if (points > top6 && !(t.getName().equals(p5))) {
-																			if (points > top5 && !(t.getName().equals(p4))) {
-																				if (points > top4 && !(t.getName().equals(p3))) {
-																					if (points > top3 && !(t.getName().equals(p2))) {
-																						if (points > top2 && !(t.getName().equals(p1))) {
-																							if (points > top1) {
-																								if ((p1 == null)) {
-																									top1 = points;
-																									p1 = t.getName();
-																									continue;
-																								}
-																							}
-																							if ((p2 == null)) {
-																								top2 = points;
-																								p2 = t.getName();
-																								continue;
-																							}
-																						}
-																						if ((p3 == null)) {
-																							top3 = points;
-																							p3 = t.getName();
-																							continue;
-																						}
-																					}
-																					if ((p4 == null)) {
-																						top4 = points;
-																						p4 = t.getName();
-																						continue;
-																					}
-																				}
-																				if ((p5 == null)) {
-																					top5 = points;
-																					p5 = t.getName();
-																					continue;
-																				}
-																			}
-																			if ((p6 == null)) {
-																				top6 = points;
-																				p6 = t.getName();
-																				continue;
-																			}
-																		}
-																		if ((p7 == null)) {
-																			top7 = points;
-																			p7 = t.getName();
-																			continue;
-																		}
-																	}
-																	if ((p8 == null)) {
-																		top8 = points;
-																		p8 = t.getName();
-																		continue;
-																	}
-																}
-																if ((p9 == null)) {
-																	top9 = points;
-																	p9 = t.getName();
-																	continue;
-																}
-															}
-															if ((p10 == null)) {
-																top10 = points;
-																p10 = t.getName();
-																continue;
-															}
-														}
-														if ((p11 == null)) {
-															top11 = points;
-															p11 = t.getName();
-															continue;
-														}
-													}
-													if ((p12 == null)) {
-														top12 = points;
-														p12 = t.getName();
-														continue;
-													}
-												}
-												if ((p13 == null)) {
-													top13 = points;
-													p13 = t.getName();
-													continue;
-												}
-											}
-											if ((p14 == null)) {
-												top14 = points;
-												p14 = t.getName();
-												continue;
-											}
-										}
-										if ((p15 == null)) {
-											top15 = points;
-											p15 = t.getName();
-											continue;
-										}
-									}
+						Map<UUID, Integer> board = sortByComparator(LevelHandler.level, true);
+						
+						for(int i = 0; i < 15; i++) {
+							if(i < board.size()) {
+								for(Entry<UUID, Integer> k : board.entrySet()) {
+									Player t = Bukkit.getPlayer(k.getKey());
+									
+									p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + t.getName() + "&7: " + k.getValue()));
 								}
 							}
 						}
-					}
-
-					if (!LevelHandler.level.isEmpty()) {
-						if (!(p1 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p1 + "&7:" + top1));
-						if (!(p2 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p2 + "&7:" + top2));
-						if (!(p3 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p3 + "&7:" + top3));
-						if (!(p4 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p4 + "&7:" + top4));
-						if (!(p5 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p5 + "&7:" + top5));
-						if (!(p6 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p6 + "&7:" + top6));
-						if (!(p7 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p7 + "&7:" + top7));
-						if (!(p8 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p8 + "&7:" + top8));
-						if (!(p9 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p9 + "&7:" + top9));
-						if (!(p10 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p10 + "&7:" + top10));
-						if (!(p11 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p11 + "&7:" + top11));
-						if (!(p12 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p12 + "&7:" + top12));
-						if (!(p13 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p13 + "&7:" + top13));
-						if (!(p14 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p14 + "&7:" + top14));
-						if (!(p15 == null)) p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9" + p15 + "&7:" + top15));
 					}
 				}
 			}
